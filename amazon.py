eb5b31f105e2a6f309259b106fab6e306b3d6ce3
mamaka <basundogel@gmail.com>
2019-06-12 16:57:53 +0800

    add avg len of reviews

--- amazon.py
+++ amazon.py
@@ -1,13 +1,16 @@
 data = []
 count = 0
+sum_len = 0
+
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line.strip())
+		data.append(line)
 		count += 1  # count = count + 1
 		if count % 1000 == 0: 
-			print(len(data)
+			print(len(data))
+print('讀取完畢,總共有', len(data), '筆資料>.^')
 
-print(len(data))
-print(data[0])
-print('----------')
-print(data[1])
+
+for d in data:
+	sum_len = sum_len + len(d)
+print('留言平均度是', sum_len / len(data))
